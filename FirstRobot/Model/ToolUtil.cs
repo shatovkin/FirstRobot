39314fc90ac71260be5be42b24166927b63e3151
Dimitri Shatovkin <dsh@covis.de>
2020-12-10 10:32:21 +0100

    FB-001

---
+++ FirstRobot/Model/ToolUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstRobot.Model
+{
+    public class ToolUtil
+    {
+        public static string messageD7 = "подход к 7 Дема: ";
+        public static string messageH7 = "подход к 7 Чема: ";
+        public static string messageD14 = "подход к 14 Дема: ";
+        public static string messageH14 = "подход к 14 Чема: ";
+    }
+}
